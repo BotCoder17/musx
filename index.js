0c1937aabc0a7eb50c269a050df5646669738fa0
idris <50110894+BotCoder17@users.noreply.github.com>
2020-08-02 02:22:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ setInterval(() => {
 
 client.login(process.env.TOKEN);
 client.on("message", msg => {
-  if(msg.content == 'play'){
+
   msg.member.voice.channel
     .join()
     .then(connection => {
@@ -32,5 +32,5 @@ client.on("message", msg => {
       const dispatcher = connection.play(broadcast);
     })
     .catch(console.error);
-  }
+  
 });
