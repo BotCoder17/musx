8f8751fcd2a8339fc46f832a570db0359e6c0bc3
idris <50110894+BotCoder17@users.noreply.github.com>
2020-08-02 21:42:36 +0530

    Create music_cmds.js

---
+++ music_cmds.js
@@ -0,0 +1,15 @@
+exports.run = (msg, cmd, client, Discord, ownerID) => {
+  if (msg.author.id == ownerID) {
+    switch (cmd) {
+      case "play":
+        let play = require("./music/play.js");
+        play.run(msg, client, Discord, ownerID);
+        break;
+      case "leave":
+      case "dc":
+        let leave = require("./music/leave.js");
+        leave.run(msg, client, Discord, ownerID);
+        break;
+    }
+  }
+};
