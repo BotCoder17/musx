ba294940ee715ed56a1651b582a1859df17e739c
idris <50110894+BotCoder17@users.noreply.github.com>
2020-08-04 20:34:24 +0530

    Update leave.js

--- music/leave.js
+++ music/leave.js
@@ -1,4 +1,6 @@
-exports.run = (msg, client, Discord) => {
+exports.run = (msg, client, Discord, ownerID) => {
+  if(msg.author.id == ownerID){
   msg.member.voice.channel.leave();
     msg.channel.send(`Left the voice channel. Cya! ${msg.author}`);
+  }
 };
